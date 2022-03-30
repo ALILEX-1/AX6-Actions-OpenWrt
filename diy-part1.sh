8b26e9013a27b5dcaa1450e8d99f344c93352f6d
hnyyghk <i@5icodes.com>
2022-03-31 01:22:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+git revert edbd8d2e9839357f3a4f0a06174d243f362b1544
