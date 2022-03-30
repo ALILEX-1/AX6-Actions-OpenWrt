3d2c573cb1b745c12bd8abadd9d6f0bdf4f8f50c
hnyyghk <i@5icodes.com>
2022-03-31 02:52:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
 git config --global user.email "i@5icodes.com"
 git config --global user.name "hnyyghk"
 git revert edbd8d2e9839357f3a4f0a06174d243f362b1544
