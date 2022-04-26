f2ef04be8691e9fd0e3ec81e2d51b9e1bb142076
hnyyghk <i@5icodes.com>
2022-04-26 18:04:36 +0800

    Update .config

--- diy-part1-5.15-robimarko.sh
+++ diy-part1-5.15-robimarko.sh
@@ -14,10 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-mkdir -p package/helloworld
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
-  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
-done
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+#echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
