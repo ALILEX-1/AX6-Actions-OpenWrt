2a4478ec93dbc80e913d481dd04055ae0d450318
hnyyghk <i@5icodes.com>
2022-04-28 19:05:46 +0800

    Update .config

--- diy-part1-5.15-robimarko.sh
+++ diy-part1-5.15-robimarko.sh
@@ -19,4 +19,7 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
-#echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
+echo 'src-git Boos https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
+
+git clone https://github.com/mchome/openwrt-vlmcsd.git package/vlmcsd
+git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
