5b0740d432618923cdf58917ac383b6c463ae194
hnyyghk <i@5icodes.com>
2022-05-05 21:42:03 +0800

    Update diy-part1.sh

--- diy-part1-5.15-robimarko.sh
+++ diy-part1-5.15-robimarko.sh
@@ -13,8 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-ls -a
-
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
