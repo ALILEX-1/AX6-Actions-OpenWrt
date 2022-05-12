ebb3211581e9d579d2dceb878d5a296bfcba7b21
hnyyghk <i@5icodes.com>
2022-05-12 15:13:21 +0800

    移除多拨支持

--- diy-part1-5.15-openwrt.sh
+++ diy-part1-5.15-openwrt.sh
@@ -43,7 +43,7 @@ done
 
 mkdir custom-feed/applications
 
-for i in "luci-app-vlmcsd" "luci-app-mwan3helper" "luci-app-syncdial"; do \
+for i in "luci-app-vlmcsd"; do \
   svn checkout "https://github.com/coolsnowwolf/luci/trunk/applications/$i" "custom-feed/applications/$i"; \
 done
 
