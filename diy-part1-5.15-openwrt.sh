9c474227536bde77ffd3ea5c48472bfff75d1db4
hnyyghk <i@5icodes.com>
2022-05-25 13:02:04 +0800

    更新commit id，显示隐藏文件

--- diy-part1-5.15-openwrt.sh
+++ diy-part1-5.15-openwrt.sh
@@ -15,25 +15,25 @@
 
 git config --global user.email "i@5icodes.com"
 git config --global user.name "hnyyghk"
-git reset 810eac8c7ffad958345f8fd02f83636afecf7fcd
+git reset 90e4c8c6e6fe060d849a5b96bc7595345ce3d6ea
 echo "openwrt: before rm -rf *"
-ls
+ls -a
 rm -rf *
 echo "openwrt: after rm -rf *"
-ls
+ls -a
 # Retain .git and .ccache
 rm -rf .gitattributes .github .gitignore
 echo "openwrt: after rm -rf .gitattributes .github .gitignore"
-ls
+ls -a
 git clone https://github.com/robimarko/openwrt -b ipq807x-5.15-pr ../openwrt-temp
 echo "openwrt-temp: before rm -rf ../openwrt-temp/.git"
-ls ../openwrt-temp
+ls -a ../openwrt-temp
 rm -rf ../openwrt-temp/.git
 echo "openwrt-temp: after rm -rf ../openwrt-temp/.git"
-ls ../openwrt-temp
+ls -a ../openwrt-temp
 mv ../openwrt-temp/* ../openwrt-temp/.[^.]* ./
 echo "openwrt: after mv ../openwrt-temp/* ../openwrt-temp/.[^.]* ./"
-ls
+ls -a
 git add -A
 git commit -m "temp"
 git pull --rebase
